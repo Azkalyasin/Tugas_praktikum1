e311aaa29ec9cbe7213cb8cb35b88fc1e9a3e3d6
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-02-26 17:27:14 +0700

    Add project files.

---
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double Njarijari, Nluas;
+
+	cout << "## Tugas praktikum pertama ##" << endl;
+	cout << "=============================" << endl;
+
+	cout << "Masukkan jari jari: ";
+	cin >> Njarijari;
+
+	Nluas = 3.14 * Njarijari * Njarijari;
+
+	cout << "Total luasnya adalah: " << Nluas << endl;
+}
